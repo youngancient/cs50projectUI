bd26150e0bd548dc9eccdc7e8803c2683dd2a909
Jude <judetochyokoye@gmail.com>
2022-10-07 18:46:24 +0100

    comment page form

--- static/comment/script.js
+++ static/comment/script.js
@@ -8,16 +8,22 @@ const nameError = document.querySelector('.sub .name');
 const commentError = document.querySelector('.sub .comment');
 const codeName = document.querySelector('#name');
 const comment = document.querySelector('#comment');
+const alumna = document.querySelector('form #alumna');
+const alumnus = document.querySelector('form #alumnus');
+const formImgs = document.querySelectorAll('form img');
 const localError = document.querySelector('.django-error');
 
 let shown = false;
 pen.onclick = ()=>{
     formDiv.classList.toggle('view');
+    formImgs.forEach((ele)=>{ele.style.display = 'none'});
     if(!shown){
         form.style.transform = 'scale(1)';
         errors.forEach((them)=>{
             them.style.visibility = 'hidden';
           });
+        const chosen = formImgs[Math.floor(Math.random() * formImgs.length)];
+        chosen.style.display = 'block';
         codeName.value = '';
         comment.value = '';
         shown = true;
@@ -48,6 +54,14 @@ comment.onkeyup =()=>{
         localError.style.visibility = 'visible';
         e.preventDefault();
     }else{
+        const data = localStorage.getItem('data');
+        const obj = JSON.parse(data);
+        if(obj.type == 'm'){
+            alumnus.style.display = 'block';
+        }
+        if(obj.type == 'f'){
+            alumna.style.display = 'block';
+        }
         if(codeName.value == "" && comment.value == ""){
             errors.forEach((them)=>{
                 them.style.visibility = "visible";
